58c8dbd47864612cc83eb6df76a3b3cec8a8becf
yhma <isyehui@gmail.com>
2025-12-14 20:30:09 +0800

    Initial Commit

---
+++ ShareBill/ShareBillApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShareBillApp.swift
+//  ShareBill
+//
+//  Created by alex_yehui on 2025/12/14.
+//
+
+import SwiftUI
+
+@main
+struct ShareBillApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
